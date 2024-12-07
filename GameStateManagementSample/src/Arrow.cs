de7e5445edb28ad8d6ffb5a01aa06a3b6ee3ad58
philmik93 <philipp.mikos@gmail.com>
2024-12-07 14:11:58 +0100

    shooting arrows and picking up health potion added

--- GameStateManagementSample/src/Arrow.cs
+++ GameStateManagementSample/src/Arrow.cs
@@ -13,11 +13,14 @@ namespace wizard_game
 {
     public class Arrow : Projectile
     {
+        Vector2 startPos;
 
-        public Arrow(int x, int y, Acteur attacker) : base(new Vector2(x, y), 10, 10, "arrow", true, attacker)
+        public Arrow(int x, int y) : base(new Vector2(x, y), 10, 10, "arrow", true)
         {
-            LoadSprite(1,1,0.12f);
-            speed = 400f;
+            sprite = new Sprite(GameStateManagementGame.Get().Content.Load<Texture2D>(spritename), 1, 1, 0.12f);
+            speed = 300f;
+            GameplayScreen.projectiles.Add(this);
+            startPos = position;
         }
 
 
@@ -27,7 +30,7 @@ namespace wizard_game
             base.Update(gameTime);
 
             if (Math.Abs(position.X-startPos.X) > 400 || Math.Abs(position.Y - startPos.Y) > 400)
-            {   
+            {
                 GameplayScreen.projectiles.Remove(this);
                 return;
             }
@@ -43,7 +46,7 @@ namespace wizard_game
                     return;
                 }
             }
-            if (DetacteCollison()) 
+            if (DetacteCollison())
             {
                 GameplayScreen.projectiles.Remove(this);
                 return;
@@ -53,9 +56,11 @@ namespace wizard_game
             float opposite = Math.Abs(direction.X);
             float adjecent = Math.Abs(direction.Y);
             float alpha = (float)Math.Atan2(opposite,adjecent);
-            
+
             if (direction.X >= 0) rotation = alpha;
             else rotation = -alpha;
+
+            if (Math.Abs(position.X-startPos.X) > 400 || Math.Abs(position.Y - startPos.Y) > 400) GameplayScreen.projectiles.Remove(this);
         }
     }
 
